8a6425cd6f07f00ac26ceea534524288874e09e3
daviddlw <davidjefiny@gmail.com>
2013-06-21 12:24:04 +0800

    装饰者模式迁入

---
+++ DecoratorProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorProject
+{
+    /// <summary>
+    /// 装饰着模式，运行时动态添加功能即插即用
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITank tank = new Tank();
+            TankAbility bfaTank = new BulletAbility(new ArmourAbility(new FlyAbility(tank)));
+            TankAbility faTank = new FlyAbility(new ArmourAbility(tank));
+
+            Console.WriteLine("This is first tank type:");
+            bfaTank.ShowTankInfo();
+            Console.WriteLine();
+            Console.WriteLine("This is second tank type:");
+            faTank.ShowTankInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DecoratorProject/Tank.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorProject
+{
+    public interface ITank
+    {
+        void ShowTankInfo();
+    }
+
+    public class Tank : ITank
+    {
+        public void ShowTankInfo()
+        {
+            Console.WriteLine("I am a normal tank!");
+        }
+    }
+
+
+    public abstract class TankAbility : ITank
+    {
+        private ITank tank;
+
+        public TankAbility(ITank tank)
+        {
+            this.tank = tank;
+        }
+
+        public virtual void ShowTankInfo()
+        {
+            tank.ShowTankInfo();
+        }
+    }
+
+    public class BulletAbility : TankAbility
+    {
+        public BulletAbility(ITank tank) : base(tank) { }
+
+        public void GetAbility()
+        {
+            Console.WriteLine("Now, I get the bullet ability!");
+        }
+
+        public override void ShowTankInfo()
+        {
+            base.ShowTankInfo();
+            GetAbility();
+        }
+    }
+
+    public class ArmourAbility : TankAbility
+    {
+        public ArmourAbility(ITank tank) : base(tank) { }
+
+        public void GetAbility()
+        {
+            Console.WriteLine("Now, I get the armour ability!");
+        }
+
+        public override void ShowTankInfo()
+        {
+            base.ShowTankInfo();
+            GetAbility();
+        }
+    }
+
+    public class FlyAbility : TankAbility
+    {
+        public FlyAbility(ITank tank) : base(tank) { }
+
+        public void GetAbility()
+        {
+            Console.WriteLine("Now, I get the fly ability!");
+        }
+
+        public override void ShowTankInfo()
+        {
+            base.ShowTankInfo();
+            GetAbility();
+        }
+    }
+
+    public class InvisibleAbility : TankAbility
+    {
+        public InvisibleAbility(ITank tank) : base(tank) { }
+
+        public void GetAbility()
+        {
+            Console.WriteLine("Now, I get the invisible ability!");
+        }
+
+        public override void ShowTankInfo()
+        {
+            base.ShowTankInfo();
+            GetAbility();
+        }
+    }
+
+}
